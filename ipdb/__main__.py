8f797c81d5a5003d33350fe655afe40c7cb286e9
Michael Petrov <800877+michaelpetrov@users.noreply.github.com>
2024-01-07 17:18:19 -0800

    skip_restart

--- ipdb/__main__.py
+++ ipdb/__main__.py
@@ -341,6 +341,8 @@ def main():
             # In most cases SystemExit does not warrant a post-mortem session.
             print("The program exited via sys.exit(). Exit status: ", end='')
             print(sys.exc_info()[1])
+            if skip_restart:
+                break
         except:
             traceback.print_exc()
             print("Uncaught exception. Entering post mortem debugging")
